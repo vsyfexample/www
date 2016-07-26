dc9d4ffa351c1b6e322f4b07c7d3050ec6b2076d
vsyf <vsyf@VB.(none)>
2016-07-27 00:06:33 +0800

    improve: 1.background 2.contacts 3.js
    Signed-off-by: vsyf <vsyf@VB.(none)>

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,7 @@
 	$(document).ready(function() {
 	$(".burger, .menu").click(function(){
 		$(this).parent().find(".hide").slideToggle();
+		$(".header_title").slideToggle();
+		$(".header_subtitle").slideToggle();
 	});
 });
